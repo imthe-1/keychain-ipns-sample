d9b49161b010e316550e5bfe6c662b1d29e4304c
imthe1 <imthe1atwork@gmail.com>
2023-07-05 15:42:38 +0530

    describes different steps of the code sample

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+// client lib of the Keychain toolset ecosystem
 import { IPFS } from '@mdip/client';
 import axios from 'axios';
 
+// Ensure these flags are set. ipfs daemon --enable-namesys-pubsub --enable-pubsub-experiment
+// Plug in IPFS node address here. Ensure the IPFS config has ws port set.
 const IPFS_NODE = 'http://localhost:5001';
 const IPFS_WS_PORT = 4001;
 
+/**
+ * IPFS is a class created by us that works as a wrapper for all the IPFS functionalities with some sanity checks added to it.
+ */
 const ctrl = new IPFS({ url: IPFS_NODE, wsPort: IPFS_WS_PORT });
 
+// Starts the browser/nodejs IPFS node and connects it to the local/remote IPFS node for ensuring propagation even after the browser/nodejs IPFS node stops.
 ctrl.connect().then(async (res) => {
   console.log('[Keychain] connect', res);
+  // Sample object. In actual implementation this will be replaced by the Keychain DID Doc.
   const doc = { toolsetName: 'Keychain', ts: new Date().toISOString() };
+  /**
+   * If "lifetime" value is not set it defaults to "3day" and reproduces the consistency problem we are facing.
+   * but if "lifetime" is set to "2min", the resolution becomes consistence
+   * End goal is to have consistency no matter what the lifetime value is and ALSO, ensure that IPNS names and records persist for very long periods of time such as 10 years or even more.
+   */
   const lifetimeObj = { lifetime: '' };
   console.log('[Keychain] doc', doc);
   const CID = await ctrl.sendJSONToIPFS([doc]);
@@ -19,18 +32,16 @@ ctrl.connect().then(async (res) => {
 
   // Publishing using the Keychain toolset modificaion
   const keypair = `keypair-${Date.now()}`;
-  const IPNSPrivateKey = '296a16e3fcea976fe7fb480580ff4fbb909858815b7644b6fb1ac092aaab3aa1';
+  // k1 private key hex. please plug in a new private key for experimentation.
+  const IPNSPrivateKey = 'dcdc545e91f559b4f070fc9048b094dbf45d4f47a8821e13f41649e2aa1a5d0d';
   const IPNSKey = await ctrl.deriveIPNSKeypair(keypair, IPNSPrivateKey);
-  console.log('[IPNSKey]', IPNSKey); // deterministic
+  // deterministic IPNS keypair generation. for the same private key, same IPNS name(pub key hash) gets generated.
+  console.log('[IPNSKey]', IPNSKey);
   const ipfsAddr = `/ipfs/${CID[0].path}`;
   try {
     // by default the lifetime is set to "3day". it can be modified by passing a 4th arg opts { lifetime: '2min' } to "linkToIPNS"
-    const ipnsLink = await ctrl.linkToIPNS(
-      ipfsAddr,
-      keypair,
-      IPNSKey.id,
-      lifetimeObj
-    );
+    const ipnsLink = await ctrl.linkToIPNS(ipfsAddr, keypair, IPNSKey.id, lifetimeObj);
+    // linkToIPNS is a wrapper for the IPNS publish function
     console.log('[Keychain IPNS Publish] name', ipnsLink.name);
     console.log('[Keychain IPNS Publish] value', ipnsLink.value);
     console.log('[Keychain] IPFS URL: https://ipfs.io' + ipnsLink.value);
@@ -42,11 +53,13 @@ ctrl.connect().then(async (res) => {
     }
     console.log('[Keychain] DID Doc data', JSON.stringify(doc));
 
+    // Resolving the published IPNS name.
     const resolvedIPNS = await ctrl.resolveIPNS(`/ipns/${ipnsLink.name}`);
     console.log('[Keychain] resolvedIPNS', resolvedIPNS);
 
     console.log('[Keychain] Does new data match resolved IPNS data(local ipfs node):', JSON.stringify(doc) === resolvedIPNS);
 
+    // for verifying if the published IPNS name resolves via ipfs.io public gateway or not.
     const IPNSConfig = {
       method: 'get',
       url: `https://ipfs.io/ipns/${ipnsLink.name}`,
@@ -59,15 +72,13 @@ ctrl.connect().then(async (res) => {
 
     console.log('[Keychain] Does local ipfs node IPNS data equal ipfs.io IPNS data: ', resolvedIPNS === JSON.stringify(respIPNSData));
   } catch (e) {
-    console.log('[EXPECTED_ERROR] in block #1 since subs happens first time', e)
+    console.log('[EXPECTED_ERROR] in block #1 since subs happens first time', e);
   }
   try {
-    const ipnsLinkII = await ctrl.linkToIPNS(
-      ipfsAddr,
-      keypair,
-      IPNSKey.id,
-      lifetimeObj
-    );
+    /**
+     * We publish and resolve here again since we were told by the IPFS devs that the PubSub subscription to IPNS name happens the first time hence it will only resolve the second time. Another issue to look at.
+     */
+    const ipnsLinkII = await ctrl.linkToIPNS(ipfsAddr, keypair, IPNSKey.id, lifetimeObj);
     console.log('[Keychain IPNS Publish]', ipnsLinkII);
     console.log('[Keychain] DID Doc data  ', JSON.stringify(doc));
 
@@ -88,7 +99,7 @@ ctrl.connect().then(async (res) => {
 
     console.log('[Keychain] Does local ipfs node IPNS data equal ipfs.io IPNS data: ', resolvedIPNSII === JSON.stringify(respIPNSDataII));
   } catch (e) {
-    console.log('[UNEXPECTED_ERROR] in block #2', e)
+    console.log('[UNEXPECTED_ERROR] in block #2', e);
   }
 });
 
@@ -97,9 +108,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          please go to console in dev tools to see the publishing and resolution logs
-        </p>
+        <p>please go to console in dev tools to see the publishing and resolution logs</p>
       </header>
     </div>
   );
